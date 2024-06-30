9a62ef5fb02dd53fd08ec38eb9e1bbfede1f560b
Martin Calderón <elmcwl@gmail.com>
2024-06-29 19:20:35 -0700

    feat: echo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>
+      <?php
+      
+        $greeting = 'Hello';
+      
+        echo "$greeting World!";
+        
+        echo "<br/>";
+    
+        echo $greeting . ', World!';
+
+      ?>
+    </h1>
+  </body>
+</html>
