6f7b3d62f85191e9fd14ada9ddfd1d6f01059f1d
Martin Calderón <elmcwl@gmail.com>
2024-06-29 20:20:25 -0700

    feat: if conditional and short echo syntax

--- index.php
+++ index.php
@@ -2,22 +2,30 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+      body {
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+      }
+    </style>
   </head>
   <body>
-    <h1>
       <?php
-      
-        $greeting = 'Hello';
-      
-        echo "$greeting World!";
+        $name = "Dark Matter";
+        $read = true;
         
-        echo "<br/>";
-    
-        echo $greeting . ', World!';
-
+        if ($read)
+          $message = "You have read $name";
+        else 
+          $message = "You have not read $name";  
+        
       ?>
+    <h1>
+      <?= $message ?>
     </h1>
   </body>
 </html>
