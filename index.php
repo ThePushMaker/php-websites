1d53698a397291b718249b363aa385f01b8553d9
Martin Calderón <elmcwl@gmail.com>
2024-07-01 22:32:29 -0700

    feat: functions and filters

--- index.php
+++ index.php
@@ -5,32 +5,53 @@
     <title>Document</title>
   </head>
   <body>
-     <h2>Associative Arrays</h2>
      <?php
       $books = [
         [
           'name' => 'Do Androids Dream of Electric Sheep?',
           'author' => 'Philip K. Dick',
+          'releaseYear' => 1968,
           'purchaseUrl' => 'http://example.com'
         ],
         [
           'name' => 'Project Hail Mary',
           'author' => 'Andy Weir',
+          'releaseYear' => 2021,
+          'purchaseUrl' => 'http://example.com'
+        ],
+        [
+          'name' => 'The Martian',
+          'author' => 'Andy Weir',
+          'releaseYear' => 2021,
           'purchaseUrl' => 'http://example.com'
         ]
       ];
+      
+      function filterByAuthor($books,  $author)
+      {
+        $filteredBooks = [];
+        
+        foreach ($books as $book) {
+            if($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        
+        return $filteredBooks; 
+      }
+      
+      
      ?>
      
      <ul>
-      <?php foreach ($books as $book): ?>
-        <li>
-          <a href="<?= $book['purchaseUrl'] ?>">
-            <?= $book['name'] ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
+      <?php foreach (filterByauthor($books, 'Andy Weir') as $book): ?>
+          <li>
+            <a href="<?= $book['purchaseUrl'] ?>">
+              <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+            </a>
+          </li>
+        <?php endforeach; ?>
      </ul>
-     
      
   </body>
 </html>
