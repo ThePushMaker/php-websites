93609dfe1fcb480b33cf40293e45de31fee5815f
Martin Calderón <elmcwl@gmail.com>
2024-07-13 19:26:31 -0700

    feat: add router

---
+++ lectures/13-router/views/contact.view.php
@@ -0,0 +1,17 @@
+<?php require('partials/head.php'); ?>
+
+<!-- navigation -->
+<?php require('partials/nav.php'); ?>
+
+
+<!-- header -->
+<?php require('partials/banner.php'); ?>
+
+<!-- content -->
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <p>Contact us now!</p>
+  </div>
+</main>
+
+<?php require ('partials/footer.php'); ?>
---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+  '/' => 'controllers/index.php',
+  '/about' => 'controllers/about.php',
+  '/contact' => 'controllers/contact.php'
+];
+
+function abort($code = 404)
+{
+  http_response_code($code);
+  require "views/{$code}.php";
+  die();
+}
+
+function routeToController($uri, $routes)
+{
+  if (array_key_exists($uri, $routes))
+    require $routes[$uri];
+  else
+  {
+    abort();
+  }
+}
+
+routeToController($uri, $routes);
